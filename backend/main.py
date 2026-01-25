af57406b0e1774b4afbadc720b8bf27e6a6c9e79
Ibrahim Ammar Onaolapo <ammaar0406@gmail.com>
2026-01-25 23:43:34 +0100

    pdftoword

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,7 @@ import os
 
 load_dotenv()
 from modules.gpa_calculator import router as gpa_calculator_router
-# TEMPORARILY DISABLED - OpenCV dependency issues on Railway
-# from modules.pdf_to_word import router as pdf_to_word_router
+from modules.pdf_to_word import router as pdf_to_word_router
 from modules.pdf_merge_split import router as pdf_merge_split_router
 from modules.pdf_compressor import router as pdf_compressor_router
 from modules.paraphraser import router as paraphraser_router
@@ -37,7 +36,7 @@ app.add_middleware(
 )
 
 app.include_router(gpa_calculator_router)
-# app.include_router(pdf_to_word_router)  # TEMPORARILY DISABLED
+app.include_router(pdf_to_word_router)
 app.include_router(pdf_merge_split_router)
 app.include_router(pdf_compressor_router)
 app.include_router(paraphraser_router)
